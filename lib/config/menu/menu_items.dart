bfb06565c9e6df12525f3b3913dea22511198b69
diegofranciscoteran <diego_teran99@hotmail.com>
2024-06-05 23:46:52 -0500

    new components

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,4 +39,14 @@ const appMenuItems = <MenuItem>[
       subtitle: "Statefull widget animado",
       link: "/animated",
       icon: Icons.check_box_outlined),
+  MenuItem(
+      title: "UI Controls + Tiles",
+      subtitle: "Serie de controles en Flutter",
+      link: "/ui-controls",
+      icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: "Introduccion a la aplicacion",
+      subtitle: "Tutorial de la app",
+      link: "/tutorial",
+      icon: Icons.accessibility_new_rounded),
 ];
